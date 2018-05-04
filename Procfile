6d9865f8d8f689f2526799ae136e391d6af4bc51
MaximZoom <max2014nas@yandex.ru>
2018-05-04 22:36:00 +0700

    okay

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 bot: python bot.py
-=======
-bot: python bot.py
->>>>>>> f76fd127d1cff0c3175b971ee91abefb2ccf4603
--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 token = '584374267:AAGHV3NCW5mgnAUk_XKaf5bIyH2TqePDGNo'
-=======
-token = '584374267:AAGHV3NCW5mgnAUk_XKaf5bIyH2TqePDGNo'
->>>>>>> f76fd127d1cff0c3175b971ee91abefb2ccf4603
